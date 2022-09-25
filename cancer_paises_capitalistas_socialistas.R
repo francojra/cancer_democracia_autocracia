eb7b78e89a11d109c68898dcb23ffb44a686c12d
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-09-24 23:36:27 -0300

    Add files via upload

--- cancer_paises_capitalistas_socialistas.R
+++ cancer_paises_capitalistas_socialistas.R
@@ -11,6 +11,22 @@
 ### fazendo dessa doença a segunda principal causa de morte, a primeira
 ### causa de morte é devido à doenças cardiovasculares.
 
+### O progresso contra muitas outras causas de morte e factores demográficos 
+### de aumento da população, da expectativa de vida e - particularmente 
+### em países de rendimento mais elevado - do envelhecimento da população 
+### significam que o número total de mortes por cancer continua a aumentar.
+### Este é um tema muito pessoal para muitos: quase todos conhecem ou perderam 
+### alguém que lhes é querido por esta doença.
 
+### Câncer é definido pelo Instituto Nacional de Câncer como uma coleção de doenças
+### em que ocorre a divisão e porpagação de células anormais nos tecidos. Câncer
+### de vários tipos pode se espalhar em muitas partes do corpo através do sangue
+### ou sistema linfático. 
+
+# Carregar pacotes -------------------------------------------------------------------------------------------------------------------------
+
+library(tidyverse)
+library(cols4all)
+library(hrbrthemes)
 
 
